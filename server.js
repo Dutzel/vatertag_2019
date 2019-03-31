6c8fb646d419bd426c523613532f0148954ff004
Dutzel <dustin.spallek@haw-hamburg.de>
2019-03-31 23:13:29 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/now-ui-kit-angular-master'));
+app.use(express.static(__dirname + '/dist/vatertag2019'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/now-ui-kit-angular-master/index.html'));
+res.sendFile(path.join(__dirname+'/dist/vatertag2019/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
